511c509359523cc6deb7273bf95da2a6a089161f
johndolac <johndolacjr@gmail.com>
2022-05-18 10:41:09 -0600

    completed sections 1,2,3

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, world")
+# Part 1
+# Installed Python, Pycharm, etc.
+
+# Part 2 Basic Syntax
+print("Welcome to Python programming");
+
+# Part 3 commentsin Python
+print("This is a comment");
+
+"""
+This is a multiple line comment
+We have to many comments
+Where are the bugs
+Welcome to the world
+"""
