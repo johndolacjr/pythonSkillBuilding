ea20672450f8700e2283a1c1d9c3041042d77f4f
johndolac <johndolacjr@gmail.com>
2022-05-20 13:11:50 -0600

    completed sub section 5

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 # Installed Python, Pycharm, etc.
 
 # Part 2 Basic Syntax
+''''
 print("Welcome to Python programming");
-
+'''
 # Part 3 commentsin Python
+'''
 print("This is a comment");
+'''
 
 """
 This is a multiple line comment
@@ -36,6 +39,21 @@ print(x)
 print(y) 
 print(z)
 """
-
 x = "Hello"
 print(type(x))
+# will print the class (str, float, or int) depending on the value type
+# variables hold a value type - can use different names: x, X, y, Y, z, Z, val, var, age, name, etc. 
+# variables are sensative - ensure lower or caps are assigned to the right values; keep it clean to avoid errors
+
+
+#SUB SECTION 5 - Rules of Naming variables
+"""Varaibles can only contain alphanumeric(a-z), 0-9, or underscores""""
+"""Variable name cant start with a number
+Must start with a letter or it can start w/ an _ and can contain an _
+example: namepak = 8 OR name_pak = 8 --> it will print 8
+"""
+age_1 = 38
+size = ("Large")
+_school = ("U of U")
+print (age_1, size)
+print (_school)
