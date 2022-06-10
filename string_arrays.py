47161bc2c195bac85595ed9a9ab28a571e23314b
johndolac <johndolacjr@gmail.com>
2022-06-10 09:21:21 -0600

    completed string array section

---
+++ string_arrays.py
@@ -0,0 +1,20 @@
+#x = "Hello World"
+
+'''
+print(x[4])
+# will print the letter "o" b/c its the 4th key in the string arrays
+'''
+'''
+for x in "Hello World":
+    print(x)
+'''
+
+'''
+x = "Hello world"
+print(len(x))
+#this will print the length of the string (11) since there are 11 characters in the length of the string
+'''
+
+x = "This is a really long sentence abot python. Wow this is so long"
+if "sentence" in x: 
+    print("Yes, sentence exists")
