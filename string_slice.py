862adc90f8665f5a7b2dc9e43669c0f4fce93f7c
johndolac <johndolacjr@gmail.com>
2022-06-10 09:34:26 -0600

    practice slicing strings

---
+++ string_slice.py
@@ -0,0 +1,15 @@
+x = "Peter Pan"
+
+'''
+print(x[1:5])
+#This code will return "eter" when the code is run. This only pulls the 4 digits as it pulls up to the 5th character but not the 5th character
+
+print(x[:6])
+#this pulls the first 6 characters...will return "Peter Pan"
+
+print(x[6:])
+#prints from the 6th character to the end of the string and
+'''
+
+print(x[-5:-1])
+#this returns characters counting from the left to the right...this example returns "r Pan"
