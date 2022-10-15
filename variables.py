a98bc2ab984ce34b8e2815dbccbe3b5f2938e53e
johndolac <johndolacjr@gmail.com>
2022-10-15 12:27:25 -0600

    worked on variables

---
+++ variables.py
@@ -0,0 +1,30 @@
+# """
+# x = "Hello"
+
+# print(x)
+# #  "Hello will print"
+
+# x = int(5)
+# y = float(7.9)
+# z = str("Name")
+
+# print(x)
+# print(y)
+# print(z)
+
+# # will print 5, 7.9, and "Name"
+# """
+
+# x = 7
+
+# print(type(x))
+
+# will print class type which is a String (STR)
+# 
+
+age = 39
+
+print(age)
+
+
+
