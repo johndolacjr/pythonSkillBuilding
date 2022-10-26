597dea81c6b252ad01c84797160eeeb2df061b93
johndolac <johndolacjr@gmail.com>
2022-10-25 18:30:21 -0600

    python skill building: escapecharacters and string formats

---
+++ escapecharacter.py
@@ -0,0 +1,19 @@
+x = 'It\'s your day'
+
+print(x)
+
+# prints "It's your day"
+
+
+y = "He said that \"He is coming\""
+
+# prints: He said that "He is coming"
+
+z = "Peter \nPaul"
+
+# prints: Peter Paul (on 2 lines 1 above the other line)
+# Peter
+# Paul
+
+a = "James \bJohn"
+# prints: JamesJohn
--- stringformat.py
+++ stringformat.py
@@ -10,4 +10,6 @@ b = 5
 c = 6.9
 y = "Your number is {}, Your second number is {}, and your third numberis {}"
 
+print(y.format(a,b,c))
+
 # will print "Your number is 2, Your second number is 5, and your third numberis 6.9"
